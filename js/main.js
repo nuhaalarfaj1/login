4c3c6b3598d3d23c78abcdeee62b297f196f61cb
NUHA ALARFAJ <nuhaalarfaj@NUHAs-MacBook-Pro.local>
2018-04-13 16:09:38 -0700

    first

---
+++ js/main.js
@@ -0,0 +1,22 @@
+var objpeople = [
+    {
+        username : "Essence",
+        password:"180"
+        
+    },
+    {
+        username : "Mera",
+        password:"150"
+    },
+    {
+        username: "Njod",
+        password:"120" 
+    },
+    {
+       username: "dody",
+        password:"110"
+    }
+];
+// login function
+
+
