68fc69676ef99feed40eca3b6786c215de598df4
NUHA ALARFAJ <nuhaalarfaj@NUHAs-MacBook-Pro.local>
2018-04-13 17:13:56 -0700

    first

--- js/main.js
+++ js/main.js
@@ -1,22 +1,44 @@
 var objpeople = [
     {
-        username : "Essence",
-        password:"180"
+        username : "essence",
+        password:"nn1"
         
     },
     {
-        username : "Mera",
-        password:"150"
+        username : "mera",
+        password:"aa1"
     },
     {
-        username: "Njod",
-        password:"120" 
+        username: "njod",
+        password:"bb1" 
     },
     {
        username: "dody",
-        password:"110"
+        password:"aa1"
     }
 ];
 // login function
-
-
+function login(){
+    // input from the form
+    var username = document.getElementById("username").Value
+    var password = document.getElementById("password").Value
+    // loop through the obj
+    for (i = 0 ; i < objpeople.length; i++){
+        if (username == objpeople[i].username && password == objpeople[i].password){
+            console.log(username + "is logged in !!");
+            return
+        };
+    }
+// if username and password not match
+console.log("incorrect username or password")
+}
+function registerUser(){
+    var registerUser = document.getElementById("newUser").Value
+    var registerPassword = document.getElementById("newPassword").Value
+    var newUser ={
+        username:registerUser,
+        password:registerPassword
+    }
+    objpeople.push(newUser)
+    console.log(objpeople)
+}
